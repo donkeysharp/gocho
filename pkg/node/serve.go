f93488af0ad35c388b5c0d11cd1a84476ae697ba
Sergio Guillen <serguimant@gmail.com>
2018-01-28 15:17:23 -0400

    Use custom ServeMux to handle the same path on different ports for dashboard and file sharing

--- pkg/node/serve.go
+++ pkg/node/serve.go
@@ -3,13 +3,15 @@ package node
 import (
 	"fmt"
 	"github.com/donkeysharp/gocho/pkg/config"
+	"io"
 	"net/http"
 	"time"
 )
 
 func fileServe(conf *config.Config) {
-	http.Handle("/", http.FileServer(http.Dir(conf.ShareDirectory)))
-	http.ListenAndServe(fmt.Sprintf("0.0.0.0:%s", conf.WebPort), nil)
+	fileMux := http.NewServeMux()
+	fileMux.Handle("/", http.FileServer(http.Dir(conf.ShareDirectory)))
+	http.ListenAndServe(fmt.Sprintf("0.0.0.0:%s", conf.WebPort), fileMux)
 }
 
 func startAnnouncer(conf *config.Config) {
@@ -19,10 +21,20 @@ func startAnnouncer(conf *config.Config) {
 	announcer.Start()
 }
 
+func dashboardServe(conf *config.Config) {
+	dashboardMux := http.NewServeMux()
+
+	dashboardMux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "foobarken")
+	})
+	http.ListenAndServe(fmt.Sprintf("localhost:%s", conf.LocalPort), dashboardMux)
+}
+
 func Serve(conf *config.Config) {
 	startAnnouncer(conf)
 
 	go fileServe(conf)
+	go dashboardServe(conf)
 
 	for {
 		time.Sleep(time.Minute * 15)
