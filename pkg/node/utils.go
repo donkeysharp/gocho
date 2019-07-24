3d6addfc3992c4059cd1b8fe72c955ed29f4ed48
Dennis Ruiz <darvein@gmail.com>
2019-07-24 16:02:08 -0400

    Formating with go fmt pkg/node/utils.go

--- pkg/node/utils.go
+++ pkg/node/utils.go
@@ -8,18 +8,18 @@ import (
 // https://stackoverflow.com/a/39324149/916063
 // open opens the specified URL in the default browser of the user.
 func openUrl(url string) error {
-    var cmd string
-    var args []string
+	var cmd string
+	var args []string
 
-    switch runtime.GOOS {
-    case "windows":
-        cmd = "cmd"
-        args = []string{"/c", "start"}
-    case "darwin":
-        cmd = "open"
-    default: // "linux", "freebsd", "openbsd", "netbsd"
-        cmd = "xdg-open"
-    }
-    args = append(args, url)
-    return exec.Command(cmd, args...).Start()
+	switch runtime.GOOS {
+	case "windows":
+		cmd = "cmd"
+		args = []string{"/c", "start"}
+	case "darwin":
+		cmd = "open"
+	default: // "linux", "freebsd", "openbsd", "netbsd"
+		cmd = "xdg-open"
+	}
+	args = append(args, url)
+	return exec.Command(cmd, args...).Start()
 }
